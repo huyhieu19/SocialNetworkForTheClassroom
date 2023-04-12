fc9defed1ec7babe2b710ef657a12ba65b7c29c2
HuyHieu <huyhieu.hust2855@gmail.com>
2023-04-12 13:21:10 +0700

    [SN-1] [BE] modify enity

---
+++ Server/SocialNetworkForClass/Database/Extentions/ModelBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Extentions
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
